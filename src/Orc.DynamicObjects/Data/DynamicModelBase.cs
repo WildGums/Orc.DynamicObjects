60dd3a25ed42fa939b2bb9cec3d07d27ee629b11
Geert van Horrik <geert@catenalogic.com>
2018-03-22 14:13:18 +0100

    Migrate to new project style

---
+++ src/Orc.DynamicObjects/Data/DynamicModelBase.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DynamicModelBase.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.DynamicObjects
+{
+    using System;
+    using System.Dynamic;
+    using System.Linq.Expressions;
+
+    using Catel.Data;
+    using Catel.Logging;
+
+    /// <summary>
+    /// Dynamic model base implementing the <see cref="IDynamicMetaObjectProvider"/>.
+    /// </summary>
+#if NET
+    [Serializable]
+#endif
+    public class DynamicModelBase : ModelBase, IDynamicMetaObjectProvider
+    {
+        /// <summary>
+        /// The log.
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Registers a simple property, which means only the name and type are required.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="type">The type.</param>
+        /// <returns>The <see cref="PropertyData"/>.</returns>
+        internal protected void RegisterDynamicProperty(string name, Type type)
+        {
+            var modelType = GetType();
+
+            if (IsPropertyRegistered(modelType, name))
+            {
+                return;
+            }
+
+            Log.Debug("Registering dynamic property '{0}.{1}'", modelType.FullName, name);
+
+            var propertyData = RegisterProperty(name, type);
+
+            InitializePropertyAfterConstruction(propertyData);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="T:System.Dynamic.DynamicMetaObject" /> responsible for binding operations performed on this object.
+        /// </summary>
+        /// <param name="parameter">The expression tree representation of the runtime value.</param>
+        /// <returns>The <see cref="T:System.Dynamic.DynamicMetaObject" /> to bind this object.</returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public DynamicMetaObject GetMetaObject(Expression parameter)
+        {
+            var metaObject = new DynamicModelBaseMetaObject(parameter, this);
+            return metaObject;
+        }
+    }
+}
