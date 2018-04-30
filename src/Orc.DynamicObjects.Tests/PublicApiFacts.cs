fd4ea12f3517bcb3e834c1f9070d0543174d542f
Geert van Horrik <geert@catenalogic.com>
2018-04-30 19:30:53 +0200

    Migrate test project to new project structure

--- src/Orc.DynamicObjects.Tests/PublicApiFacts.cs
+++ src/Orc.DynamicObjects.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.DynamicObjects.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_DynamicObjects_HasNoBreakingChanges()
         {
             var assembly = typeof(DynamicModelBase).Assembly;
